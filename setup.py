5696866446d053c3e97fad5a2bce13916493ce4e
redddyyashwanth04 <yashwanthreddy0415@gmail.com>
2025-09-07 23:55:08 +0530

    application created

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="yashwanth",
+    author_email="sunnyreddy96767@gmail.com",
+    packages=find_packages(),
+)
--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,1 +1,61 @@
-print("hey")
+import pandas as pd
+import numpy as np
+from src.logger.logging import logging
+from src.exception.exception import customexception
+
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionConfig:
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+
+    def initiate_data_ingestion(self):
+        logging.info("data ingestion ")
+        try:
+            data=pd.read_csv("C://Users//yashw//Downloads//archive (6)//Diamonds Prices2022.csv")
+            logging.info(" reading a df")
+
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
+            
+            logging.info("here i have performed train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
+        except Exception as e:
+            logging.info(f"Data ingestion failed: {e}") # FIXED: Log the actual error
+            raise customexception(e, sys)
+
+
+if __name__=="__main__":
+    obj=DataIngestion()
+
+    obj.initiate_data_ingestion()
